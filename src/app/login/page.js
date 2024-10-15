fb30337e8c874409c176155b491a2cb47b1ef51a
vpsingh16 <144575378+vpsingh16@users.noreply.github.com>
2024-10-15 07:26:01 +0530

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -7,7 +7,10 @@ import { ToastContainer, toast, Slide } from 'react-toastify';
 const Login = () => {
     const { data: session } = useSession()
     const router = useRouter()
-    document.title = "Login - Thanks-Chat"
+    useEffect(() => {
+        // Set the document title inside useEffect to ensure it runs on the client
+        document.title = "Login - Thanks-Chat"
+    }, []);
 
     useEffect(() => {
         if (session) {
