02ff967bccf5f4d192fec8b2ab5e974af9b64eb1
vpsingh16 <144575378+vpsingh16@users.noreply.github.com>
2024-10-15 07:03:08 +0530

    Add files via upload

---
+++ src/app/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear()
+    return (
+        <footer className="bg-gray-900 text-white flex items-center justify-center px-4 h-16">
+           <p className="text-center">Copyright &copy;{currentYear} Thanks-Chat | All Right Reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
