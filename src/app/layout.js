a0b82d93445adea1a8fef9b57eb050419705c86e
vpsingh16 <144575378+vpsingh16@users.noreply.github.com>
2024-10-15 07:00:25 +0530

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import SessionWrapper from "./components/SessionWrapper";
+import 'react-toastify/dist/ReactToastify.css';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Thanks Chat - Get your project funded",
+  description: "A crowdfunding platform for getting your project funded",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className=" bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+        <SessionWrapper>
+          <Navbar />
+          <div className="text-white min-h-screen">
+            {children}
+          </div>
+          <Footer />
+        </SessionWrapper>
+      </body>
+    </html>
+  );
+}
